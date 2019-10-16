bf7f83bc92a2a03a1e1058a231de3f0a069cfba9
AmarKingso <39182884+AmarKingso@users.noreply.github.com>
2019-10-16 23:41:55 +0800

    Add files via upload

---
+++ Game5-两款物理引擎相关游戏/HitUFO/Assets/Scripts/SSActionManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SSActionManager : MonoBehaviour{
+	Dictionary<int, SSAction> executing = new Dictionary<int, SSAction>();
+	List<SSAction> waitToDo = new List<SSAction>();
+	List<int> waitToDestroy = new List<int>();
+
+	protected void Update() {
+		foreach(SSAction act in waitToDo) {
+			executing[act.GetInstanceID()] = act;
+		}
+		waitToDo.Clear();
+
+		foreach(KeyValuePair<int, SSAction> kv in executing) {
+			SSAction act = kv.Value;
+			//当前动作做完就该被销毁，否则继续执行
+			if (act.destroy)
+				waitToDestroy.Add(act.GetInstanceID());
+			else
+				act.Update();
+		}
+
+		foreach(int key in waitToDestroy) {
+			SSAction act = executing[key];
+			executing.Remove(key);
+			Destroy(act);
+		}
+
+		waitToDestroy.Clear();
+	}
+
+	public void runAction(GameObject gameobject,SSAction action,ISSActionCallBack manager) {
+		action.gameobject = gameobject;
+		action.transform = gameobject.transform;
+		action.callback = manager;
+		waitToDo.Add(action);
+		action.Start();
+	}
+}
