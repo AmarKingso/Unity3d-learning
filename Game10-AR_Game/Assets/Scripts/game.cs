a383fd6457996c9e2267afe9fd7a81aaee0f8a12
Amar_so <460450237@qq.com>
2019-12-23 19:29:09 +0800

    Game10-AR

---
+++ Game10-AR_Game/Assets/Scripts/game.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class game : MonoBehaviour, IVirtualButtonEventHandler
+{
+    private GameObject sphere;
+    public float step = 1f;
+    private Color[] color = { Color.red, Color.blue, Color.yellow, Color.black };
+    private System.Random rd = new System.Random();
+
+    void IVirtualButtonEventHandler.OnButtonPressed(VirtualButtonBehaviour vbb) {
+        int index = (int)rd.Next(4);
+        sphere.GetComponent<MeshRenderer>().material.color = color[index];
+        Debug.Log(color[index]);
+    }
+
+    void IVirtualButtonEventHandler.OnButtonReleased(VirtualButtonBehaviour vbb) {
+        Debug.Log("released");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        VirtualButtonBehaviour[] vbbs = GetComponentsInChildren<VirtualButtonBehaviour>();
+        foreach(var vbb in vbbs) {
+            vbb.RegisterEventHandler(this);
+        }
+
+        sphere = transform.Find("Sphere").gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
